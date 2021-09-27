c1260f4cff785c999fb4ff2c986d2477d4f3f2c0
Lislaine Souza Lima <66922432+Lislaine-souza@users.noreply.github.com>
2021-09-27 20:51:19 -0300

    Executando em outros browsers

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,14 +1,18 @@
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TesteGoogle {
 	
 	@Test
 	public void teste() {
-		System.setProperty("webdriver.gecko.driver", "C:\\Github\\Selenium-iniciante02\\driver\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.gecko.driver", "C:\\Github\\Selenium-iniciante02\\driver\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new InternetExplorerDriver();
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
 	}

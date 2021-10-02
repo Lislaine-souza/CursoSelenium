789fe92b4c555c61dd1e5ea4bfa8c24d8e694ccb
Lislaine Souza Lima <66922432+Lislaine-souza@users.noreply.github.com>
2021-10-02 19:42:25 -0300

    Continuando busca de elementos básicos:
    Combo de múltipla escolha
    Botão
    Link
    Div e Span (buscar textos)

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.validator.PublicClassValidator;
 import org.openqa.selenium.By;
@@ -21,8 +22,8 @@ public class TesteCampoTreinamento {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys("Teste");
 		Assert.assertEquals("Teste", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
 		
-		 //define um TimeOut para em caso de não encontrar elementos na página.
-		 // driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+//		define um TimeOut para em caso de não encontrar elementos na página.
+//		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		  driver.close();
 		
 	}
@@ -33,7 +34,7 @@ public class TesteCampoTreinamento {
 		driver.manage().window().setSize(new Dimension(1200, 1000));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("teste area\na letra n pula linha\nacho que isso");
-		//Assert.assertEquals("teste area", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+//		Assert.assertEquals("teste area", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
 		
 		
 		driver.close();
@@ -71,11 +72,11 @@ public class TesteCampoTreinamento {
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
 		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
 		Select combo = new Select(element);
-		//Os index começam do zero
-		//combo.selectByIndex(2);
-		//Ou podemos passar o valor do elemneto
-		//combo.selectByValue("superior");
-		//Ou selecionar valor visivel
+//		Os index começam do zero
+//		combo.selectByIndex(2);
+//		Ou podemos passar o valor do elemneto
+//		combo.selectByValue("superior");
+//		Ou selecionar valor visivel
 		combo.selectByVisibleText("2o grau completo");
 		Assert.assertEquals("2o grau completo",combo.getFirstSelectedOption().getText());
 		
@@ -103,6 +104,64 @@ public class TesteCampoTreinamento {
 		}
 		
 		Assert.assertTrue(encontrou);	
+	}
+	
+	@Test
+	public void deveVerificarValoresComboMultiplo() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 1000));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(element);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("Corrida");
+		combo.selectByVisibleText("O que eh esporte?");
+		
+		java.util.List<WebElement> allSelectedOpitions = combo.getAllSelectedOptions();
+		Assert.assertEquals(3, allSelectedOpitions.size());
+		
+		combo.deselectByVisibleText("Corrida");
+		allSelectedOpitions = combo.getAllSelectedOptions();
+		Assert.assertEquals(2, allSelectedOpitions.size());
+		
+		driver.close();
+	}
+	
+	@Test
+	public void deveInteragirComBotoes() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 1000));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement botao = driver.findElement(By.id("buttonSimple"));
+		botao.click();
+		Assert.assertEquals("Obrigado!", botao.getAttribute("value"));
+		
+	}
+	
+	@Test
+//  usamos @ignore para o teste não ser executado.
+//  @Ignore
+	public void deveInteragirComLinks() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.linkText("Voltar")).click();
+		Assert.assertEquals("Voltou!", driver.findElement(By.id("resultado")).getText());
+				
+	}	
+	
+	@Test
+	public void deveBuscarTextosNaPagina() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 1000));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+				
+//		Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("Campo de treinamento"));
+		
+		Assert.assertEquals("Cuidado onde clica, muitas armadilhas...", driver.findElement(By.className("facilAchar")).getText());
+		
+		
+		
 	}
 
 }

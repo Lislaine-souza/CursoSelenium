962952b30f324ddf331b85443d07fcf3c2661efd
Lislaine Souza Lima <66922432+Lislaine-souza@users.noreply.github.com>
2021-10-10 17:15:36 -0300

    Update TesteCampoTreinamento.java

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,17 +1,11 @@
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.validator.PublicClassValidator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
-import com.sun.tools.javac.util.List;
 
 public class TesteCampoTreinamento {
 	@Test
